36eeaf0a5cdc4407a7f204b7e2a0678209adf8b7
Masanao Imai <masa7351@gmail.com>
2020-01-18 16:58:04 +0900

    Fix Comile Error of TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import MediumEditor from 'medium-editor';
+import '../node_modules/medium-editor/dist/css/themes/default.css';
 
 const App: React.FC = () => {
+  // const editor = new MediumEditor(/*dom, */ '.medium-editable', {
+  //   autoLink: true,
+  //   delay: 1000,
+  //   targetBlank: true,
+  //   toolbar: {
+  //     buttons: [
+  //       'bold',
+  //       'italic',
+  //       'quote',
+  //       'underline',
+  //       'anchor',
+  //       'h1',
+  //       'h2',
+  //       'h3',
+  //       'h4',
+  //       'h5',
+  //       'h6',
+  //       'strikethrough',
+  //       'subscript',
+  //       'superscript',
+  //       'pre',
+  //       'image',
+  //       'html',
+  //       'justifyCenter'
+  //     ],
+  //     diffLeft: 25,
+  //     diffTop: 10
+  //   },
+  //   anchor: {
+  //     placeholderText: 'Type a link',
+  //     customClassOption: 'btn',
+  //     customClassOptionText: 'Create Button'
+  //   },
+  //   paste: {
+  //     cleanPastedHTML: true,
+  //     cleanAttrs: ['style', 'dir'],
+  //     cleanTags: ['label', 'meta'],
+  //     unwrapTags: ['sub', 'sup']
+  //   },
+  //   anchorPreview: {
+  //     hideDelay: 300
+  //   },
+  //   placeholder: {
+  //     text: 'Tell your story...'
+  //   }
+  // });
+  const editor = new MediumEditor('.editable', {
+    buttonLabels: 'fontawesome'
+  });
+
+  const [text, setText] = useState('');
+
+  const changeHandler = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
+    setText(e.currentTarget.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        className="editable medium-editor-textarea"
+        value={text}
+        onChange={changeHandler}
+      />
+      {/* <MediumEditor
+        className="medium-editable"
+        text={text}
+        options={editor}
+        onChange={changeHandler}
+      /> */}
     </div>
   );
-}
+};
 
 export default App;
